8b4c06907a532b1060f541d00797cfb5c3c573f2
gabe-gardyn <gabriel@gardyn.tech>
2024-03-23 18:47:52 -0400

    installed bulma

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,11 @@
+import "bulma";
+
 function Home() {
    return(
       <div className="container">
-         <h1>Home Function</h1>
+         <h1 className="title is-4">Home Function</h1>
+         <p className="subtitle is-6">Test</p>
+         <button className="button is-warning">Test</button>
       </div>
    );
 }
