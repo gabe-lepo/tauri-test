b2bdeab79106a04cdb54793e4dbfc023939029ec
gabe-gardyn <gabriel@gardyn.tech>
2024-03-24 18:45:48 -0400

    Setup basic home page with connect frontend

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,11 +2,31 @@ import "bulma";
 
 function Home() {
    return(
-      <div className="container">
-         <h1 className="title is-4">Home Function</h1>
-         <p className="subtitle is-6">Test</p>
-         <button className="button is-warning">Test</button>
-      </div>
+      <body className="has-background-dark">
+         <div className="block container">
+            <div className="box">
+               <p className="title is-spaced">Home</p>
+               <span className="subtitle is-4">Menu</span>
+               <nav className="breadcrumb" aria-label="breadcrumbs">
+                  <ul className="subtitle is-6">
+                     <li><a href="#">Link 1</a></li>
+                     <li><a href="#">Link 2</a></li>
+                     <li><a href="#">Link 3</a></li>
+                  </ul>
+               </nav>
+            </div>
+         </div>
+         <div className="container">
+            <div className="box">
+               <input
+                  className="input is-info"
+                  type="text"
+                  placeholder="IP Address"
+               />
+               <button className="button is-success">Connect</button>
+            </div>
+         </div>
+      </body>
    );
 }
 
