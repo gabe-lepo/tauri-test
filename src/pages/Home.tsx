25cc70afe9daba2e18771de2f837a8c6343e0e65
gabe-gardyn <gabriel@gardyn.tech>
2024-03-25 00:57:17 -0400

    remove unnecessary box div

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,18 +7,20 @@ import WifiList from "../components/WifiList";
 function Home() {
    return(
       <section className="section">
-         <div className="block container">
-            <div className="box">
-               <p className="title is-spaced">Home</p>
-               <span className="subtitle is-4">Menu</span>
-               <nav className="breadcrumb" aria-label="breadcrumbs">
-                  <ul className="subtitle is-6">
-                     <li><a href="#">Link 1</a></li>
-                     <li><a href="#">Link 2</a></li>
-                     <li><a href="#">Link 3</a></li>
-                  </ul>
-               </nav>
-            </div>
+         <div className="block container box">
+            <p className="title is-spaced">Home</p>
+            <span className="subtitle is-4">Menu</span>
+            <nav className="breadcrumb" aria-label="breadcrumbs">
+               <ul className="subtitle is-6">
+                  <li><a href="#">Link 1</a></li>
+                  <li><a href="#">Link 2</a></li>
+                  <li><a href="#">Link 3</a></li>
+               </ul>
+            </nav>
+         </div>
+         <div className="block container box">
+            <p className="title">Title</p>
+            <p>{}</p>
          </div>
 
          <WifiList />
