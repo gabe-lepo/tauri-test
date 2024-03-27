4451168a5973d71e52387dfb7912c2bc24680ec7
gabe-gardyn <gabriel@gardyn.tech>
2024-03-27 00:54:48 -0400

    Added AppControls component, update allowlist for the relevant controls

--- src/components/AppControls.tsx
+++ src/components/AppControls.tsx
@@ -1,13 +1,29 @@
 import "bulma";
 
-import { appWindow } from "@tauri-apps/api/window";
+import { relaunch, exit } from "@tauri-apps/api/process";
 
 function AppControls() {
-   
+   function reloadApp() {
+      relaunch().then((response) => {
+         console.log("@@@ AppControls:", (response))
+      }).catch((err) => {
+         console.error("@@@ AppControls: Error -", (err));
+      })
+   }
+   function closeApp() {
+      exit()
+   }
    
    return(
       <div className="block container box">
-         
+         <p className="title">App Controls</p>
+         <div className="field has-addons">
+            <p className="control">
+            <button className="button is-warning is-outlined" onClick={reloadApp}>Reload</button>
+            </p><p className="control">
+            <button className="button is-danger is-outlined" onClick={closeApp}>Exit</button>
+            </p>
+         </div>
       </div>
    );
 }
--- src/components/WifiList.tsx
+++ src/components/WifiList.tsx
@@ -5,7 +5,7 @@ function WifiList() {
    return(
       <div className="block container">
          <div className="box">
-            <p className="title">Wifi Info</p>
+            <p className="title">Wifi List</p>
             <p>test</p>
          </div>
       </div>
