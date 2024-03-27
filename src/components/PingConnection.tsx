0097e3d1c12ee4220d1a072937c7eff238a8ac23
gabe-gardyn <gabriel@gardyn.tech>
2024-03-26 23:43:05 -0400

    add box div to parent div

--- src/components/PingConnection.tsx
+++ src/components/PingConnection.tsx
@@ -8,37 +8,35 @@ function PingConnection() {
    const onPing = useCallback(async () => {postPing({})}, [[postPing]]);
 
    return(
-      <div className="block container">
-         <div className="box">
-            <p className="title">Test Connection</p>
-            <div className="block">
-               <input
-                  className="input is-info"
-                  type="text"
-                  placeholder="Enter IP Address"
-               />
-            </div>
-            <table className="table">
-               <thead><tr>
-                  <td>
-                     <p className="subtitle">Controls</p>
-                  </td>
-                  <td>
-                     <p className="subtitle">Results</p>
-                  </td>
-               </tr></thead>
-               <tbody><tr>
-                  <td>
-                     <button className="button is-success" onClick={onPing}>Ping</button>
-                  </td>
-                  <td>
-                     <p className="subtitle">
-                        {typeof usePostPing().data?.code}: {usePostPing().data?.code}
-                     </p>
-                  </td>
-               </tr></tbody>
-            </table>
+      <div className="block container box">
+         <p className="title">Test Connection</p>
+         <div className="block">
+            <input
+               className="input is-info"
+               type="text"
+               placeholder="Enter IP Address"
+            />
          </div>
+         <table className="table">
+            <thead><tr>
+               <td>
+                  <p className="subtitle">Controls</p>
+               </td>
+               <td>
+                  <p className="subtitle">Results</p>
+               </td>
+            </tr></thead>
+            <tbody><tr>
+               <td>
+                  <button className="button is-success" onClick={onPing}>Ping</button>
+               </td>
+               <td>
+                  <p className="subtitle">
+                     {typeof usePostPing().data?.code}: {usePostPing().data?.code}
+                  </p>
+               </td>
+            </tr></tbody>
+         </table>
       </div>
    );
 }
