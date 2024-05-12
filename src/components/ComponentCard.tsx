5ba1c4e1033b1d920700b45e606e481b67e5e8e1
gabe-gardyn <gabriel@gardyn.tech>
2024-05-12 00:48:11 -0400

    add transition to cards

--- src/components/ComponentCard.tsx
+++ src/components/ComponentCard.tsx
@@ -9,7 +9,8 @@ export default function ComponentCard({ component }: CardProps) {
       <div className={
          "container px-4 py-2 " +
          "min-h-32 max-h-80 overflow-auto overscroll-none border " +
-         "shadow-lg shadow-neutral-300 hover:shadow-neutral-500 "
+         "shadow-lg shadow-neutral-300 hover:shadow-neutral-500 " +
+         "transition duration-150 ease-in-out"
       }>
          <div className="border border-dashed">{component}</div>
       </div>
