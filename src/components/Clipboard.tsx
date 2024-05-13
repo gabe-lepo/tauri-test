53ab8ba36e66038e65ea2a096f77493e5c68e4b1
gabe-gardyn <gabriel@gardyn.tech>
2024-05-13 17:29:42 -0400

    Added read function to clipboard, reorganized elements

--- src/components/Clipboard.tsx
+++ src/components/Clipboard.tsx
@@ -6,31 +6,64 @@ import Button from "./Button";
 
 export default function Clipboard() {
    const [input, setInput] = useState<string>("");
+   const [output, setOutput] = useState<string | null>("");
+   const [isLoading, setIsLoading] = useState<boolean>(false);
    
    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
       setInput(event.target.value);
    };
 
+   async function writeToClipboard() {
+      setIsLoading(true)
+      await writeText(input).then((resp) => {
+         console.log(`${resp}`);
+      }).catch((err) => {
+         console.error(`${err}`);
+      }).finally(() => {
+         setIsLoading(false);
+      });
+   }
+
+   async function readFromClipboard() {
+      setIsLoading(true);
+      setOutput(await readText());
+      setIsLoading(false);
+   }
+
    return (
       <>
          <CardTitle title={"Clipboard"} subtitle="System Clipboard Interface" />
-         <input
-            className="bg-inherit w-full"
-            type="text"
-            // value={input}
-            placeholder="Enter some text"
-            autoCapitalize="off"
-            autoComplete="off"
-            autoCorrect="off"
-            onChange={handleInputChange}
-         />
-         <p>You entered: {input}</p>
-         <Button
-            type={"info"}
-            text={"Write"}
-            onClick={async () => {await writeText(input)}}
-            disabled={false}
-         />
+         <div className="grid grid-cols-2 gap-2">
+            <input
+               className="bg-inherit w-full"
+               type="text"
+               // value={input}
+               placeholder="Enter some text"
+               autoCapitalize="off"
+               autoComplete="off"
+               autoCorrect="off"
+               onChange={handleInputChange}
+            />
+            <Button
+               type={"info"}
+               text={"Copy"}
+               onClick={writeToClipboard}
+               disabled={false}
+            />
+            <input
+               className="bg-inherit w-full"
+               placeholder="Clipboard contents"
+               type="textarea"
+               value={output?.toString()}
+               disabled
+            />
+            <Button
+               type={"info"}
+               text={"Read"}
+               onClick={readFromClipboard}
+               disabled={isLoading}
+            />
+         </div>
       </>
    )
 };
