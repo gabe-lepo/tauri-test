4e85482812e778fa8847079c61cd960012d47fc6
gabe-gardyn <gabriel@gardyn.tech>
2024-05-13 14:07:27 -0400

    add basic shell command function

--- src/components/Shell.tsx
+++ src/components/Shell.tsx
@@ -1,10 +1,31 @@
 import "../main.css";
+import Button from "./Button";
 import CardTitle from "./CardTitle";
+import { Command } from "@tauri-apps/api/shell";
 
 export default function Shell() {
+   async function runCommand() {
+      const command = new Command('pwd');
+      command.on('close', data => {
+         console.log(`Command finished with code ${data.code} and signal ${data.signal}`)
+      });
+      command.on('error', error => console.error(`command error: "${error}"`));
+      command.stdout.on(`data`, line => console.log(`command stdout: "${line}"`));
+      command.stderr.on(`data`, line => console.log(`command stderr: "${line}"`));
+
+      const child = await command.spawn();
+      console.log(`pid:`, child.pid);
+   };
+
    return (
       <>
          <CardTitle title="Shell" subtitle="Interact with system shell" />
+         <Button 
+            type={"success"}
+            text={"Run Command"}
+            onClick={runCommand}
+            disabled={false}
+         />
       </>
    );
 };
