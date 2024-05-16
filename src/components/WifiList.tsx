18226538f08203b1a5f4ef93018f60ee381228c1
gabe-gardyn <gabriel@gardyn.tech>
2024-05-16 00:17:37 -0400

    use basic rust function in frontend

--- src/components/WifiList.tsx
+++ src/components/WifiList.tsx
@@ -1,12 +1,43 @@
 import "../main.css";
+import { useEffect, useState } from "react";
+import { invoke } from "@tauri-apps/api/tauri";
 import CardTitle from "./CardTitle";
 
 function WifiList() {
+   const [name, setName] = useState<string>("<you>");
+   const [result, setResult] = useState<string>("");
+
+   const fetchData = async () => {
+      try {
+         const resp = await invoke<string>('hello_world', { name });
+         setResult(resp);
+      } catch (err) {
+         console.error(`Error: `, (err));
+      }
+   };
+
+   useEffect(() => {
+      fetchData();
+   }, [name]);
+
+   const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setName(e.target.value);
+   }
 
    return(
       <>
          <CardTitle title="Wifi List" subtitle="WIP" />
-         <i>TODO</i>
+         <div>
+            <input
+               type="text"
+               placeholder="Enter your name"
+               autoCapitalize="false"
+               autoComplete="false"
+               autoCorrect="false"
+               onChange={handleNameChange}
+            />
+         </div>
+         <p>{result}</p>
       </>
    );
 }
